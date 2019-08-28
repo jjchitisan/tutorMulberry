7091d607744eb972f17feeedc67225810af301ec
chitisan <jjchitisan@gmail.com>
2019-08-28 14:53:53 +0700

    router aou1

--- src/router.js
+++ src/router.js
@@ -53,6 +53,5 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/aou1.vue')
-    }
-  ]
+    }]
 })
